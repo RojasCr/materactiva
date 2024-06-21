744c0f0eeac9a9d7999aa07c4e99ea70c831c289
Cristian Rojas <cristianrojas781@gmail.com>
2024-06-20 22:50:30 -0300

    update 20-6-2024 v5

---
+++ src/server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.use(express.static(`${process.cwd()}/src/public`));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${process.cwd()}/src/views/inicio.html`);
+});
+
+app.get("/polinomios", (req, res) => {
+    res.sendFile(`${process.cwd()}/src/views/polinomios.html`);
+});
+app.get("/ecuaciones", (req, res) => {
+    res.sendFile(`${process.cwd()}/src/views/ecuaciones.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Server listen on port ${port}`);
+});
